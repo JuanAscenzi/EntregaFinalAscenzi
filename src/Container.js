f321e89cc6b5a897871370b27c1cb8f9ebe42030
Juan Ignacio Ascenzi Márquez <ascenzimarquezjuanignacio@gmail.com>
2023-08-29 01:38:47 -0300

    Container actualizado

--- src/Container.js
+++ src/Container.js
@@ -1,12 +1,27 @@
-// import React, { useState } from 'react'
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { Presentacional } from "./Presentacional"
+
 
 export function Container() {
+  
+  const [ data,setData ] = useState([])
+  const resultado = useParams()
+  
+  
+  useEffect(() => {
+    
+    getProductos()
+    if(resultado.id){
+      console.log("Estoy en la ruta categorias" + resultado.id)
+    }else{
+      console.log("Estoy en Home")
+    }
+    
+  },[resultado.id])
+  
 
-    //const [ data,setData ] = useState([])
-
-
-
-  return (
-    <div></div>
+  return(
+    <Presentacional data={data}/>
   )
 }
